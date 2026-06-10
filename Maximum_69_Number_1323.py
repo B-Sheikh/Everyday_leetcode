f0a7ed03be546a67d3680d82e100f90bb3927d4d
Abhishek Bharathi <148848587+B-Sheikh@users.noreply.github.com>
2026-06-10 21:52:24 +0530

    Create Maximum_69_Number_1323.py
    https://leetcode.com/problems/maximum-69-number/?envType=problem-list-v2&envId=dr6e2be1

---
+++ Maximum_69_Number_1323.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def maximum69Number(self, num):
+        s = str(num)
+        for i in range(len(s)):
+            if s[i] == '6':
+                return int(s[:i] + '9' + s[i+1:])
+        return num
